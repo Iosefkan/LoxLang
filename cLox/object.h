99bcde7d1b654b59a6d8ddf658c02bc86b4d0a0b
George <vsedletski@gmail.com>
2025-09-15 21:05:02 +0300

    closures

--- cLox/object.h
+++ cLox/object.h
@@ -10,16 +10,20 @@
 #define IS_STRING(value) isObjType(value, OBJ_STRING)
 #define IS_FUNCTION(value) isObjType(value, OBJ_FUNCTION)
 #define IS_NATIVE(value) isObjType(value, OBJ_NATIVE)
+#define IS_CLOSURE(value) isObjType(value, OBJ_CLOSURE)
 
 #define AS_STRING(value) ((ObjString*)AS_OBJ(value))
 #define AS_CSTRING(value) (((ObjString*)AS_OBJ(value))->chars)
 #define AS_FUNCTION(value) ((ObjFunction*)AS_OBJ(value))
 #define AS_NATIVE(value) (((ObjNative*)AS_OBJ(value))->function)
+#define AS_CLOSURE(value) (((ObjClosure*)AS_OBJ(value)))
 
 typedef enum{
     OBJ_STRING,
     OBJ_FUNCTION,
-    OBJ_NATIVE
+    OBJ_NATIVE,
+    OBJ_CLOSURE,
+    OBJ_UPVALUE
 } ObjType;
 
 struct Obj {
@@ -30,6 +34,7 @@ struct Obj {
 typedef struct{
     Obj obj;
     int arity;
+    int upvalueCount;
     Chunk chunk;
     ObjString* name;
 } ObjFunction;
@@ -48,7 +53,23 @@ struct ObjString {
     uint32_t hash;
 };
 
+typedef struct ObjUpvalue{
+    Obj obj;
+    Value* location;
+    Value closed;
+    struct ObjUpvalue* next;
+} ObjUpvalue;
+
+typedef struct{
+    Obj obj;
+    ObjFunction* function;
+    ObjUpvalue** upvalues;
+    int upvalueCount;
+} ObjClosure;
+
+ObjClosure* newClosure(ObjFunction* function);
 ObjFunction* newFunction();
+ObjUpvalue* newUpvalue(Value* slot);
 ObjNative* newNative(NativeFn function);
 ObjString* takeString(char* chars, int length);
 ObjString* copyString(const char* chars, int length);
