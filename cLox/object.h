df3c4fda3d01a1c9a5b216f735434bd6eaeb06f7
George <vsedletski@gmail.com>
2025-09-21 01:10:49 +0300

    superclass

--- cLox/object.h
+++ cLox/object.h
@@ -12,7 +12,7 @@
 #define IS_FUNCTION(value) isObjType(value, OBJ_FUNCTION)
 #define IS_NATIVE(value) isObjType(value, OBJ_NATIVE)
 #define IS_CLOSURE(value) isObjType(value, OBJ_CLOSURE)
-#define IS_CLAS(value) isObjType(value, OBJ_CLASS)
+#define IS_CLASS(value) isObjType(value, OBJ_CLASS)
 #define IS_INSTANCE(value) isObjType(value, OBJ_INSTANCE)
 
 #define AS_STRING(value) ((ObjString*)AS_OBJ(value))
